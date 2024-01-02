e4c8ee29385ab83bd6c24273d0ee1ca53cb66850
lucas <lucas.savioz@laplateforme.io>
2024-01-02 14:43:44 +0100

    modification du job06

--- jour01/job06.py
+++ jour01/job06.py
@@ -1,13 +1,21 @@
 class Animal:
-    def __init__(self, age):
+    def __init__(self, age, name):
         self.age = age
-        
+        self.name = name
+
     def vieillir(self):
         self.age += 1
         return "L'age de l'animal est " + str(self.age) + "."
 
-p1 = Animal(0)
+    def nommer(self):
+        return "L'animal se nomme " + self.name
 
-result1 = p1.vieillir()
+p1 = Animal(0, "Luna")
+
+result1 = p1.vieillir() + " " + p1.nommer()
+
 
 print(result1)
+
+
+
